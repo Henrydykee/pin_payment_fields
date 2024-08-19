9b03f9024d36e6f5da908952b2d6c8c91c09f891
henrydykee <dykeehenry@gmail.com>
2024-08-19 21:45:00 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// function createTextField(id, name, placeholder) {
+//     const container = document.getElementById('textFieldContainer');
+//     const label = document.createElement('label');
+//     label.setAttribute('for', id);
+//     label.textContent = name;
+
+//     const input = document.createElement('input');
+//     input.type = 'text';
+//     input.id = id;
+//     input.name = name;
+//     input.placeholder = placeholder;
+
+//     container.appendChild(label);
+//     container.appendChild(input);
+// }
+
